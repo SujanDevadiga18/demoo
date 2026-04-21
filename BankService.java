a97fc72cfdae0af3147a41e668f304bc82653255
Sujan <sujandevadiga607@gmail.com>
2026-04-21 10:20:01 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -1,7 +1,7 @@
 public class BankService{
   public static void main(String[] args){
     int balance = 1000;
-    int deposit = 600;
+    int deposit = 500;
     int withdraw = 300;
 
     balance = balance + deposit;
